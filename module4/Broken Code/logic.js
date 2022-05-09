822a05e4a45680dda9bed759cc4f48bc714a4685
MLyjak1 <mlyjak@gmail.com>
2022-05-09 10:29:37 -0400

    Made all corrections to Module 4.

--- module4/Broken Code/logic.js
+++ module4/Broken Code/logic.js
@@ -3,10 +3,10 @@ $("#factButton").on("click", function() {
 	$("#factText").text(booFacts[number])
 })
 
-var booFacts = ["Boo is a pomeranian, Boo's best friend is another pomeranian named Buddy, Boo the Pomeranian was born on March 16, making him a Pisces, Boo's favourite food is grass, Boo has released two books" ]
+var booFacts = ["Boo is a pomeranian, Boo's best friend is another pomeranian named Buddy", "Boo the Pomeranian was born on March 16, making him a Pisces", "Boo's favourite food is grass", "Boo has released two books" ]
 
 $("#textPink").on("click", function() {
-	$("#funText").css("color", pink)
+	$("#funText").css("color", "pink")
 })
 
 $("#textOrange").on("click", function() {
@@ -17,12 +17,12 @@ $("#textGreen").on("click", function() {
 })
 
 // When the boxGrow button is clicked... 
-$("#boxGrow").on(click, function() {
+$("#boxGrow").on("click", function() {
 	// Increase the size of the box.
 	$("#box").animate({height:"+=35px", width:"+=35px"}, "fast");
 })
 // When the boxShrink button is clicked...
-$("#boxShrink").on(click, function() {
+$("#boxShrink").on("click", function() {
 	// Decrease the size of the box.
 	$("#box").animate({height:"-=35px", width:"-=35px"}, "fast");
 })
